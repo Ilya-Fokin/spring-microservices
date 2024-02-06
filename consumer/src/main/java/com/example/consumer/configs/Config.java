aa092dc7fc3125a944c4bdbf1e760e30c35af051
Ilya-Fokin <fokin3349@mail.ru>
2024-02-06 20:00:39 +0400

    last changes

--- consumer/src/main/java/com/example/consumer/configs/Config.java
+++ consumer/src/main/java/com/example/consumer/configs/Config.java
@@ -5,21 +5,22 @@ import com.example.consumer.models.Data;
 import com.example.consumer.services.DataServiceImpl;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.kafka.clients.admin.NewTopic;
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.common.serialization.StringDeserializer;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.autoconfigure.kafka.KafkaProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.annotation.KafkaListener;
-import org.springframework.kafka.config.KafkaListenerContainerFactory;
+import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
 import org.springframework.kafka.config.TopicBuilder;
 import org.springframework.kafka.core.ConsumerFactory;
-import org.springframework.kafka.listener.ConcurrentMessageListenerContainer;
+import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
+import org.springframework.kafka.support.serializer.JsonDeserializer;
 import org.springframework.messaging.handler.annotation.Payload;
 
-import java.util.List;
-
-import static org.springframework.kafka.support.serializer.JsonDeserializer.TYPE_MAPPINGS;
+import java.util.HashMap;
+import java.util.Map;
 
 @Slf4j
 @Configuration
@@ -49,7 +50,7 @@ public class Config {
                 topics = "${application.kafka.topic}"
         )
         public void listener(@Payload DataDto data) {
-            log.info("Получено сообшение: {}", data.toString());
+            log.info("Получено сообщение: {}", data.toString());
             //dataService.save(data);
         }
     }
--- consumer/src/main/java/com/example/consumer/repo/DataRepo.java
+++ consumer/src/main/java/com/example/consumer/repo/DataRepo.java
@@ -1,7 +1,6 @@
 package com.example.consumer.repo;
 
 import com.example.consumer.models.Data;
-import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
